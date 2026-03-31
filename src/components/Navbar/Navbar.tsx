c778030570855ee18d7b9226e9b1bcd2e6396cd4
GuramiDavitadze <guramidavitadze2002@gmail.com>
2026-04-01 02:27:43 +0400

    responsive navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="flex top-0 z-10 items-center justify-between p-4 bg-white border-b border-slate-200 sticky">
       <div className="text-xl font-bold text-indigo-600">DevLogo</div>
@@ -14,9 +17,40 @@ const Navbar = () => {
           Contact
         </a>
       </div>
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="md:hidden p-2 text-slate-600 hover:bg-slate-100 rounded-lg transition"
+      >
+        {isOpen ? <div>Close</div> : <div>Open</div>}
+      </button>
       <button className="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 cursor-pointer active:scale-95 transition-all">
         Get Started
       </button>
+      <div
+        className={`${isOpen ? "block" : "hidden"} md:hidden w-3xs bg-white border-b border-slate-200 p-4 space-y-4 absolute right-0 top-0`}
+      >
+        <a
+          href="#home"
+          onClick={() => setIsOpen(false)}
+          className="block text-slate-600 font-medium"
+        >
+          Home
+        </a>
+        <a
+          href="#projects"
+          onClick={() => setIsOpen(false)}
+          className="block text-slate-600 font-medium"
+        >
+          Projects
+        </a>
+        <a
+          href="#contact"
+          onClick={() => setIsOpen(false)}
+          className="block text-slate-600 font-medium"
+        >
+          Contact
+        </a>
+      </div>
     </nav>
   );
 };
