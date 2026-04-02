5d6fa5019b2c3ba32e6af102ed132e8b3171f5ba
GuramiDavitadze <guramidavitadze2002@gmail.com>
2026-04-02 04:06:56 +0400

    Responsive is doen

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,28 +10,56 @@ const Navbar = () => {
     <nav className="shadow-md rounded-xl md:shadow-none md:rounded-none w-full py-3">
       <div className="flex justify-between items-center h-13 max-w-6xl w-[90%] mx-auto ">
         <div className="font-bold text-3xl">Area</div>
-        <div
-          className={`${isOpen ? "flex" : "hidden"} flex-col w-full h-100 bg-white fixed absolute top-17 left-0 z-10 md:items-center md:grow md:justify-between md:static`}
-        >
+        <div className={`hidden md:flex items-center grow justify-between`}>
           <div className="hidden md:block"></div>
-          <div className="flex flex-col items-center gap-10">
+          <div className="flex items-center gap-10">
             <a href="#">Benefits</a>
             <a href="#">Specifications</a>
             <a href="#">How-to</a>
             <a href="#">Contact Us</a>
           </div>
-          <button className="flex items-center mx-auto mt-5 border  px-2 py-2 rounded-full bg-[#485C11] text-white cursor-pointer">
+          <button className="flex items-center px-2 py-2 rounded-full bg-[#485C11] text-white cursor-pointer">
             <span>Learn More</span>
             <MdArrowOutward className="text-xs color-white" />
           </button>
         </div>
-        <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+        <button
+          className="md:hidden cursor-pointer"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           {isOpen ? (
             <IoMdClose className="text-3xl" />
           ) : (
             <IoIosMenu className="text-3xl" />
           )}
         </button>
+        <div
+          className={`${isOpen ? "flex" : "hidden"} md:hidden items-center grow justify-between absolute top-16 left-0 flex-col bg-white w-full`}
+        >
+          <div className="hidden md:block"></div>
+          <div className="flex flex-col gap-10 w-[90%]">
+            <div className="w-full bg-black bg-slate-300  h-[1px]"></div>
+            <a href="#" onClick={() => setIsOpen(!isOpen)}>
+              Benefits
+            </a>
+            <div className="w-full bg-black bg-slate-300  h-[1px]"></div>
+            <a href="#" onClick={() => setIsOpen(!isOpen)}>
+              Specifications
+            </a>
+            <div className="w-full bg-black bg-slate-300  h-[1px]"></div>
+            <a href="#" onClick={() => setIsOpen(!isOpen)}>
+              How-to
+            </a>
+            <div className="w-full bg-black bg-slate-300  h-[1px]"></div>
+            <a href="#" onClick={() => setIsOpen(!isOpen)}>
+              Contact Us
+            </a>
+          </div>
+          <button className="flex items-center mt-10 px-2 py-2 rounded-full bg-[#485C11] text-white cursor-pointer">
+            <span>Learn More</span>
+            <MdArrowOutward className="text-xs color-white" />
+          </button>
+        </div>
       </div>
     </nav>
   );
